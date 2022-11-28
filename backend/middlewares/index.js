88c7a6523da003a5d58598b23ce3383cca53c656
Hammad Hameed <hammadhameed956@gmail.com>
2022-11-28 14:21:11 +0500

    all necessary middlewares added

--- backend/middlewares/index.js
+++ backend/middlewares/index.js
@@ -1,15 +1,18 @@
-const  helmet  = require('helmet');
-const {authorize} = require('./authverify');
-const {isAdmin} = require('./isAdmin');
+const helmet = require('helmet');
+const { authorize } = require('./authverify');
+const { isAdmin } = require('./isAdmin');
 const { log_saver, logger } = require('./logger');
 const { myCorsPolicy } = require("./cors");
 const { limiter } = require("./limiter");
 
-module.exports ={
+const compression = require("compression");
+
+module.exports = {
     authorize,
-    isAdmin,log_saver,
+    isAdmin, log_saver,
     logger,
     helmet,
     myCorsPolicy,
-    limiter
+    limiter,
+    compression
 }
